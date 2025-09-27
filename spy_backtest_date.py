29ca7d9fff17f6b1bc0ad2765be417259da01ea4
hol0gramX <80895815+hol0gramX@users.noreply.github.com>
2025-09-26 21:58:17 -0400

    Update spy_backtest_date.py

--- spy_backtest_date.py
+++ spy_backtest_date.py
@@ -181,7 +181,7 @@ def backtest(start_date_str, end_date_str):
     for s in signals: print(s)
 
 if __name__=="__main__":
-    backtest("2025-09-03","2025-09-03")
+    backtest("2025-09-25","2025-09-26")
 
 
 
